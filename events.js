7740939e1a89d9918e002a5264b24a5a7f716f90
Bruno Pereira <b.alexandre.p@outlook.com>
2018-06-26 18:18:38 +0100

    Create link with array info

--- events.js
+++ events.js
@@ -1,14 +1,12 @@
 function addEvent(id, name, description, startTime, endTime) {
 
-    console.log(id, name, description, startTime, endTime);
-    /*var txt1 = "<b>I </b>";                   // Create element with HTML
-    var txt2 = $("<i></i>").text("love ");    // Create with jQuery
-    var txt3 = document.createElement("b");   // Create with DOM
-    txt3.innerHTML = "jQuery!";
-    $("img").after(txt1, txt2, txt3);      // Insert new elements after img*/
+    
     let element = document.createElement('div');
     element.className = 'event';
-    element.innerHTML = name;
+    let linkName = document.createElement('a');
+    linkName.innerHTML = name;
+    linkName.href = "https://www.facebook.com/events/" + id;
+    element.appendChild(linkName);
     document.getElementById('eventList').appendChild(element);
 }
 const xhr = new XMLHttpRequest();

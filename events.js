bb59396ca2ada592ecd70bc52b86096f1b3ac210
pedropcruz <pedropcruz@outlook.pt>
2018-07-02 21:59:00 +0100

    changes on js for es6 syntax

--- events.js
+++ events.js
@@ -1,6 +1,4 @@
-function addEvent(id, name, description, startTime, endTime) {
-
-    
+let addEvent = (id, name, description, startTime, endTime) => {
     let element = document.createElement('div');
     element.className = 'event';
     let linkName = document.createElement('a');
@@ -9,15 +7,16 @@ function addEvent(id, name, description, startTime, endTime) {
     element.appendChild(linkName);
     document.getElementById('eventList').appendChild(element);
 }
+
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://graph.facebook.com/v3.0/freeCodeCampLisbon/events');
 xhr.setRequestHeader('Authorization', 'Bearer EAAUOkpBroAwBAKaUQJ8TGjZBJZBVAce7dPkB9GwMmHgksdVob3ZBZCX28eFAkGd1mbkApc3VF0SEQu655mNesbc2MqWaLZA4BhTwNbmyDcRKFAzswTlqrpoY4WHRTpxcttAMPlpavah4MaZAFIgqWMZBAZC4GesPn2MZD');
 xhr.send();
 
-xhr.onload = function(e){
+xhr.onload = (e) => {
     let valuesRequest = JSON.parse(xhr.response);
-     console.log(valuesRequest.data);
-     valuesRequest.data.forEach(function(elements){
+    console.log(valuesRequest.data);
+    valuesRequest.data.forEach((elements) => {
         addEvent(elements.id, elements.name, elements.description, elements.startTime, elements.endTime);
-     });
+    });
 }
